fff741e054de2f9be78b762710716ed93a2b8b63
Prithyangaa Senthil <163582023+Prithyangaa@users.noreply.github.com>
2025-09-27 10:41:53 +0100

    Creating cache file

--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from fastf1 import plotting
 import matplotlib
 
 def setup():
+    if not os.path.exists("cache"):
+        os.makedirs("cache")
     fastf1.Cache.enable_cache('cache')  # create a local cache folder
-    plotting.setup_mpl(misc_mpl_mods=False)  # optional: improve colors
+    plotting.setup_mpl(misc_mpl_mods=False) 
     matplotlib.rcParams['figure.figsize'] = [10, 6]
